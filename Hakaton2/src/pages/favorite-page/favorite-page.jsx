c8e02dac3d0e60a7e68934032501958e17498a61
Sergei Ratnikov <sergei.ratnikoff@yandex.ru>
2025-03-23 11:22:14 +0300

    ---

--- Hakaton2/src/pages/favorite-page/favorite-page.jsx
+++ Hakaton2/src/pages/favorite-page/favorite-page.jsx
@@ -1,15 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { MemberCard } from '../../components';
 
 export const Favorites = () => {
   const [favorites, setFavorites] = useState(
     JSON.parse(localStorage.getItem('favorites')) || []
   );
-
-  // useEffect(() => {
-  //   const storeFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  //   setFavorites(storeFavorites);
-  // }, []);
 
   const handleRemoveFromFavorites = (id) => {
     const updatedFavorites = favorites.filter((fav) => fav.id !== id);
@@ -20,17 +15,23 @@ export const Favorites = () => {
   return (
     <div className="max-w-[1000px] w-full m-auto p-4 sm:px-6">
       <h1 className="text-4xl text-center font-bold mb-4">Избранное</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-6">
-        {favorites.map((member) => (
-          <MemberCard
-            key={member.id}
-            member={member}
-            favorites={favorites}
-            isFavoritePage={true}
-            onRemoveFromFavorites={handleRemoveFromFavorites}
-          />
-        ))}
-      </div>
+      {favorites.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-6">
+          {favorites.map((member) => (
+            <MemberCard
+              key={member.id}
+              member={member}
+              favorites={favorites}
+              isFavoritePage={true}
+              onRemoveFromFavorites={handleRemoveFromFavorites}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="text-2xl text-center italic text-gray-400">
+          Избранные пока не объявились, но Морфиус и Тринити уже ищут их.
+        </div>
+      )}
     </div>
   );
 };
